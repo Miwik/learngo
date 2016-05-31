2e8224ac1fdf87b5e86e9ac29eb8a08a1c6dead1
Miwik <rem.bourreau@gmail.com>
2016-05-31 23:41:46 +0200

    finished the go tour, testing basics things

---
+++ variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func print(args ...interface{}) {
+	for i, a := range args {
+		fmt.Printf("#%d (%T) %v\n", i, a, a)
+	}
+}
+
+func main() {
+	a := "initial"
+	b, c := 1, 2
+	d, e, f := false, "plop", 1.25
+	print(a, b, c, d, e, f)
+}
